1e2d3d1f736eadbd9b375f050c69ff63c7e0957e
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-12-20 21:48:09 +0300

    add registration path

--- hoodproject/urls.py
+++ hoodproject/urls.py
@@ -16,15 +16,15 @@ Including another URLconf
 from django.contrib.auth import views 
 from django.contrib import admin
 from django.urls import path,include
-# from django_registration.backends.one_step.views import RegistrationView
+from django_registration.backends.one_step.views import RegistrationView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('hoodapp.urls')),
-    # path('accounts/register/', RegistrationView.as_view(success_url='/create_profile'),name='django_registration_register'),
-    # path('accounts/', include('django_registration.backends.one_step.urls')),
-    # path('accounts/', include('django.contrib.auth.urls')),
-    # path('logout/', views.logout_then_login, name='logout'), 
+    path('accounts/register/', RegistrationView.as_view(success_url='/create_profile'),name='django_registration_register'),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('logout/', views.logout_then_login, name='logout'), 
     
     
     
