437e176c25c64a483f5104f58d769ad1a2e1fbac
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-12-20 21:54:34 +0300

    add profile class

--- hoodapp/models.py
+++ hoodapp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+import datetime as dt
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_photo = CloudinaryField("image")
+    bio = models.TextField(max_length=300)
+    contact = models.CharField(max_length=100)
+
+    def save_profile(self):
+        self.save()
+    
+    def create_profile(self):
+        self.save()
+
+    def update_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def filter_by_id(cls, id):
+        profile = Profile.objects.filter(user=id).first()
+        return profile
+
+    def __str__(self):
+        return self.user.username
+
