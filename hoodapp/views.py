cfc27e04ea32319e08e9cb8e847e82e4391d1f50
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-12-20 21:49:20 +0300

    add create_profile function

--- hoodapp/views.py
+++ hoodapp/views.py
@@ -1,6 +1,29 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+from hoodapp.forms import ProfileForm
+
+
+
+@login_required(login_url="/accounts/login/")
 def index(request):
     
     return render(request, 'index.html')
+
+@login_required(login_url='/accounts/login/')
+def create_profile(request):
+    current_user = request.user
+    title = "Create Profile"
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+        return HttpResponseRedirect('/')
+
+    else:
+        form = ProfileForm()
+    return render(request, 'create_profile.html', {"form": form, "title": title})
 
