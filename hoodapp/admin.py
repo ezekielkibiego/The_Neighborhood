1e125e5ba9999e77f6d1cc156d34966fcb9c0bc9
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-12-30 22:29:01 +0300

    register neighborhood class

--- hoodapp/admin.py
+++ hoodapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Profile,NeighborHood,Location
 
-# Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(NeighborHood)
+admin.site.register(Location)
