f14553b1fdce39a97ef85ccbbf1212c4d7372ab0
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-12-30 22:30:51 +0300

    add neighborhood class

--- hoodapp/models.py
+++ hoodapp/models.py
@@ -4,11 +4,58 @@ from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
 
 
+
+class Location(models.Model):
+    name = models.CharField(max_length=30)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def save_location(self):
+        self.save()
+
+    def __str__(self):
+        return self.name
+
+class NeighborHood(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    occupants_count = models.IntegerField(default=0)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True,null=True)
+    updated_on = models.DateTimeField(auto_now=True,null=True)
+
+    def create_neigborhood(self):
+        self.save()
+
+    @classmethod
+    def delete_neighborhood(cls, id):
+        cls.objects.filter(id=id).delete()
+
+    @classmethod
+    def update_neighborhood(cls, id):
+        cls.objects.filter(id=id).update()
+
+    @classmethod
+    def search_by_name(cls, search_term):
+        hood = cls.objects.filter(name__icontains=search_term)
+        return hood
+
+    @classmethod
+    def find_neigborhood(cls, id):
+        hood = cls.objects.get(id=id)
+        return hood
+
+    def __str__(self):
+        return self.name
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_photo = CloudinaryField("image")
     bio = models.TextField(max_length=300)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE,null=True)
+    neighborhood = models.ForeignKey(NeighborHood, on_delete=models.CASCADE,null=True)
     contact = models.CharField(max_length=100)
+    created_on = models.DateTimeField(auto_now_add=True,null=True)
+    updated_on = models.DateTimeField(auto_now=True,null=True)
 
     def save_profile(self):
         self.save()
