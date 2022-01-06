f199d00061cdd115b5afbbdcdf1cc842bbb46594
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2022-01-06 21:03:10 +0300

    register business class

--- hoodapp/admin.py
+++ hoodapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Profile,NeighborHood,Location
+from .models import Business, Profile,NeighborHood,Location
 
 
 admin.site.register(Profile)
 admin.site.register(NeighborHood)
 admin.site.register(Location)
+admin.site.register(Business)
