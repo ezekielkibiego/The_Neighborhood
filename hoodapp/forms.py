a05b86f907f80ede4c2f5d58df6a8aca23c44869
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2022-01-06 21:03:56 +0300

    add BusinessForm class

--- hoodapp/forms.py
+++ hoodapp/forms.py
@@ -1,4 +1,5 @@
-from .models import Profile
+from django.db.models import fields
+from .models import Business, NeighborHood, Profile
 from django.forms import ModelForm
 from django import forms
 
@@ -12,3 +13,13 @@ class UpdateProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('bio', 'profile_photo','contact','location','neighborhood')
+
+class HoodForm(forms.ModelForm):
+    class Meta:
+        model=NeighborHood
+        fields = ['photo','name','content','occupants_count','location']
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model=Business
+        fields=['business_name','email','description','location','neighborhood']
