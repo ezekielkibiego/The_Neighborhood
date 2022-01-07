447d3a6c2ab494be8bfd577e946d719afd891cec
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2022-01-08 02:41:20 +0300

    add post class

--- hoodapp/models.py
+++ hoodapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 import datetime as dt
 from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
-from django.db.models.fields import TextField
+from django.db.models.fields import NullBooleanField, TextField
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 
@@ -25,12 +25,18 @@ class NeighborHood(models.Model):
     content = models.TextField(max_length=600, null=True)
     location = models.ForeignKey(Location, on_delete=models.CASCADE,null=True)
     occupants_count = models.IntegerField(default=0)
-    admin = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
     created_on = models.DateTimeField(auto_now_add=True,null=True)
     updated_on = models.DateTimeField(auto_now=True,null=True)
+    health_cell = models.IntegerField(null=True, blank=True)
+    police_hotline = models.IntegerField(null=True, blank=True)
 
     def create_neigborhood(self):
         self.save()
+    
+    def update_neighborhood(self):
+        self.update()
+
 
     @classmethod
     def delete_neighborhood(cls, id):
@@ -54,7 +60,7 @@ class NeighborHood(models.Model):
         return self.name
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile',null=True)
-    profile_photo = CloudinaryField("image")
+    profile_photo = CloudinaryField("image",null=True)
     bio = models.TextField(max_length=300)
     location = models.ForeignKey(Location, on_delete=models.CASCADE,null=True)
     neighborhood = models.ForeignKey(NeighborHood, on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
@@ -74,8 +80,43 @@ class Profile(models.Model):
     def save_user_profile(sender, instance, **kwargs):
         instance.profile.save()
 
+class Post(models.Model):
+    title = models.CharField(max_length=50,null=True)
+    content = models.TextField(blank=True, null=True)
+    photo = CloudinaryField("image",blank=True,null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE,null=True)
+    neighborhood = models.ForeignKey(NeighborHood, on_delete=models.CASCADE, default=1)
+    created_on = models.DateTimeField(auto_now_add=True,null=True)
+    updated_on = models.DateTimeField(auto_now=True,null=True)
+    
+    def create_post(self):
+        self.save()
 
+    
+    def delete_post(self):
+        self.delete()
+
+    
+    def update_post(self):
+        self.update()
+
+    
+    @classmethod
+    def search_by_title(cls, search_term):
+        post = cls.objects.filter(title__icontains=search_term)
+        return post
+
+    
+    @classmethod
+    def find_post(cls, id):
+        post = cls.objects.get(id=id)
+        return post
+
+    def __str__(self):
+        return self.title
 class Business(models.Model):
+    photo = CloudinaryField("image",null=True)
     business_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
     description = models.TextField(blank=True, null=True)
