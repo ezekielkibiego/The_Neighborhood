1a2a6f0417337ad9a9b11db224e9b9bb885836c2
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2022-01-08 03:01:31 +0300

    test location instance

--- hoodapp/tests.py
+++ hoodapp/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import *
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.location = Location(name='Test Location')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.location, Location))
+
+   
