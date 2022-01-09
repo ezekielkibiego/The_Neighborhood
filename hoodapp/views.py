f2535d14222228d673b7614cabf84f4347166ee6
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2022-01-09 18:30:27 +0300

    add profiles view function

--- hoodapp/views.py
+++ hoodapp/views.py
@@ -32,10 +32,11 @@ def index(request):
 def profile(request):
     current_user = request.user
     profile = Profile.objects.filter(user_id=current_user.id).first()
+    
     neighborhood = NeighborHood.objects.all()
     businesses = Business.objects.all().order_by('-id')
     posts = Post.objects.filter(user_id=current_user.id)
-    return render(request, "profile.html", {"profile": profile, ' neighborhood':  neighborhood, 'businesses': businesses,"posts": posts})
+    return render(request, "profile.html", {"profile": profile, ' neighborhood':  neighborhood, 'businesses': businesses,"posts": posts,})
 
 
 @login_required(login_url='/accounts/login/')
@@ -115,15 +116,22 @@ def create_hood(request):
 def hood(request):
     current_user = request.user
     hood = NeighborHood.objects.all().order_by('-id')
-    return render(request, 'hoods.html', {'hood': hood})
+    return render(request, 'hoods.html', {'hood': hood,'current_user':current_user})
+
 
 @login_required(login_url='/accounts/login/')
 def single_hood(request,name):
     current_user = request.user
     hood = NeighborHood.objects.get(name=name)
-    businesses = Business.objects.filter(user_id=current_user.id)
-    posts = Post.objects.filter(user_id=current_user.id)
-    return render(request,'single_hood.html',{'hood':hood,'businesses': businesses,"posts": posts})
+    profiles = Profile.objects.filter(neighborhood=hood)
+    businesses = Business.objects.filter(neighborhood=hood)
+    posts = Post.objects.filter(neighborhood=hood)
+    request.user.profile.neighborhood = hood
+    request.user.profile.save()
+    
+    return render(request,'single_hood.html',{'hood': hood,'businesses':businesses,
+                                                            'posts':posts,'current_user':current_user,
+                                                            'profiles':profiles})
 login_required(login_url="/accounts/login/")
 def join_hood(request,id):
     neighborhood = get_object_or_404(NeighborHood, id=id)
@@ -185,3 +193,9 @@ def search(request):
     else:
         message = "You haven't searched for any term"
         return render(request, "search.html", {"message": message})
+
+@login_required(login_url="/accounts/login/")
+def profiles(request):
+    current_user = request.user
+    profiles = Profile.objects.all().order_by('-id')
+    return render(request, 'profiles.html', {'profiles': profiles,'current_user':current_user})
